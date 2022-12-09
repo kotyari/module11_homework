00a533dff12f38fe227c283889aa824c7ede7f9d
kotyari <yarostrapesnikov@gmail.com>
2022-12-09 21:15:11 +0500

    done, without node modules
    Не добавлял папку node, т.к. очень много файлов, где то в курсе вскользь упоминалось, что так делают. Напишите, если это неправильною(Можно в Слаке)

--- src/__tests__/repeatWord.test.js
+++ src/__tests__/repeatWord.test.js
@@ -1,9 +1,29 @@
-import {repeatWord} from "./__tests__/repeatWord.js"
+import {repeatWord} from "../repeatWord.js";
 
 describe ("tests for repeatWord function", () => {
 
   it ("should operate correctly with one word", () => {
-    expect(repeatWord("try", 1)).toBe("try1")
+    expect(repeatWord("try", 1)).toBe("try1.")
+  }),
+
+  it ("should operate correctly with many words", () => {
+    expect(repeatWord("try", 5)).toBe("try1, try2, try3, try4, try5.")
+  }),
+
+  it ("should return only numbers if you are not add word", () => {
+    expect(repeatWord("", 10)).toBe("1, 2, 3, 4, 5, 6, 7, 8, 9, 10.")
+  }),
+  
+  it ("should operate correctly with 0(return empty)", () => {
+    expect(repeatWord("try", 0)).toBe("")
+  }),
+
+  it ("should operate correctly with 0 and no words(return empty)", () => {
+    expect(repeatWord("", 0)).toBe("")
+  }),
+
+  it ("should return empty with negative numbers", () => {
+    expect(repeatWord("try", -5)).toBe("")
   })
 
 
--- src/repeatWord.js
+++ src/repeatWord.js
@@ -2,7 +2,10 @@ export function repeatWord(word, count) {
     let words='';
     for (let i = 1; i<=count;i++)
       {
-       
+       if(i===count){
+        words += word + i + '.';
+        return(words)
+       }
         words += word + i + ', ';
         
       }
